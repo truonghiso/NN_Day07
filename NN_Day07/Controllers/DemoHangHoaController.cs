67bcb7c4ab4fafc667ca1e1e663729edd59fb66a
NhatNghe_Day07_UpLoadFile <truong.nguyennhat1997@gmail.com>
2020-05-31 10:53:52 +0700

    Upload File

---
+++ NN_Day07/Controllers/DemoHangHoaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NN_Day07.Models;
+
+namespace NN_Day07.Controllers
+{
+    public class DemoHangHoaController : Controller
+    {
+        static List<HangHoa> dsHangHoa = new List<HangHoa>();
+        public IActionResult Index()
+        {
+            return View(dsHangHoa);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoa hh, IFormFile Image)
+        {
+            if (Image != null)
+            {
+                var fileName = $"{DateTime.Now.Ticks}_{Image.FileName}";
+                var fullPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "products", fileName);
+                hh.Image = fileName;
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    Image.CopyTo(file);
+                    dsHangHoa.Add(hh);
+                    return Redirect("/DemoHangHoa");    //Đường dẫn " " nằm bên folder Controller
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ NN_Day07/Controllers/DemoUploadController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NN_Day07.Controllers
+{
+    public class DemoUploadController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        public IActionResult UploadSingleFile(IFormFile myfile)
+        {
+            if (myfile != null)
+            {
+                var filename = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", myfile.FileName);
+
+                //var file = new FileStream(filename, FileMode.Create);
+                //myfile.CopyTo(file);
+                //file.Close();
+
+                using (var filestream = new FileStream(filename, FileMode.Create))
+                {
+                    myfile.CopyTo(filestream);
+                }
+            }
+            return RedirectToAction("Upload", "DemoUpload");
+        }
+
+        public IActionResult UploadMultipleFile(List<IFormFile> mymultiple)
+        {
+            if (mymultiple != null)
+            {
+                foreach (var fileUpload in mymultiple)
+                {
+                    var filename = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Documents", DateTime.Now.Ticks.ToString() + fileUpload.FileName);
+
+                    //var file = new FileStream(filename, FileMode.Create);
+                    //myfile.CopyTo(file);
+                    //file.Close();
+
+                    using (var filestream = new FileStream(filename, FileMode.Create))
+                    {
+                        fileUpload.CopyTo(filestream);
+                    }
+                }
+            }
+            return RedirectToAction("Upload", "DemoUpload");
+        }
+    }
+}
+
